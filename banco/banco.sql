130b3b5484d141abdd1a703d940843e524613980
Shiver30 <fernandoramos30ice@gmail.com>
2026-09-03 12:42:03 -0300

    Update banco.sql

--- banco/banco.sql
+++ banco/banco.sql
@@ -5783,8 +5783,11 @@ VALUES ('Usuario Teste', 'teste@email.com', '123456', '2000-05-15', '12345678900
 
 INSERT INTO usuarios
 (usuarios_nome, usuarios_email, usuarios_senha, usuarios_idade, usuarios_cpf, usuarios_sexo, usuario_img)
-VALUES ('Adm', 'adm@gmail.com', 'adm123', '01-01-01', '067-011-069-77', 'm', '../fotos/upadr.jpeg');  
+VALUES ('Adm', 'adm@gmail.com', 'adm123', '01-01-01', '067-011-069-77', 'm', '../fotos/upadr.jpeg');
 
+INSERT INTO usuarios
+(usuarios_nome, usuarios_email, usuarios_senha, usuarios_idade, usuarios_cpf, usuarios_sexo, usuario_img)
+VALUES ('Sandy', 'sandy@gmail.com', 'sandy123', '2067-01-28', '067-011-069-69', 'f', '../fotos/upadr.jpeg');
 
 DROP TABLE IF EXISTS `banco`.`view1`;
 USE `banco`;
